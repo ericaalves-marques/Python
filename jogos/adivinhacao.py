fd3c00184228f7a121811e9d20f1e317c800d804
Érica Alves Marques <119536121+ericaalves-marques@users.noreply.github.com>
2023-10-01 19:07:51 -0300

    Correção

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,21 +1,25 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao Jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
 rodada = 1
 
+print(numero_secreto)
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativas ", rodada, "de ", total_de_tentativas)
-    chute = input("Digite um número entre 1 e 100: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
 
-if chute < 1 or chute >100:
-    print("Você deve digitar um número entre 1 e 100")
-    continue
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100")
+        continue
 
-    print("Você digitou ", chute)
-    chute = int(chute)
+
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
