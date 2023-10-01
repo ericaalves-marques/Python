43c7eb7fe9ebaa28f96f6e3b82d8ed43824baa87
Érica Alves Marques <119536121+ericaalves-marques@users.noreply.github.com>
2023-09-30 23:53:23 -0300

    Erro

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -8,7 +8,11 @@ rodada = 1
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativas ", rodada, "de ", total_de_tentativas)
-    chute = input("Digite o seu número: ")
+    chute = input("Digite um número entre 1 e 100: ")
+
+if chute < 1 or chute >100:
+    print("Você deve digitar um número entre 1 e 100")
+    continue
 
     print("Você digitou ", chute)
     chute = int(chute)
@@ -19,10 +23,11 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if acertou:
         print("Você acertou!")
+        break
     else:
         if maior:
             print("Seu chute foi maior do que o número secreto!")
             print("Você errou!")
         if menor:
             print("Seu chute foi menor do que o número secreto!")
-    print("Fim do Jogo!")
+print("Fim do Jogo!")
