30d90c2405d54e644db9caa07824d39d79445df6
Érica Alves Marques <119536121+ericaalves-marques@users.noreply.github.com>
2023-10-04 16:01:05 -0300

    Codigo forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,6 +4,14 @@ def jogar_forca():
     print("Bem vindo ao Jogo de Forca!")
     print("*********************************")
 
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("Jogando... ")
+
     print("Fim do Jogo!")
 
 if __name__=="__main__":
