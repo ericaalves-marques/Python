d3388274f25d953c0816e091228b7cbaff17e977
Érica Alves Marques <119536121+ericaalves-marques@users.noreply.github.com>
2023-10-10 09:16:18 -0300

    Encerrando o jogo

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,13 +5,35 @@ def jogar_forca():
     print("*********************************")
 
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_","_","_","_","_","_"]
+
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
-        print("Jogando... ")
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
 
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
     print("Fim do Jogo!")
 
 if __name__=="__main__":
