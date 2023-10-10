07bcf282442419d0e02439286e44d67c511912c9
Érica Alves Marques <119536121+ericaalves-marques@users.noreply.github.com>
2023-10-10 09:50:07 -0300

    Random

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,11 +1,23 @@
+import random
+
 def jogar_forca():
 
     print("*********************************")
     print("Bem vindo ao Jogo de Forca!")
     print("*********************************")
 
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
 
-    palavra_secreta = "banana".upper()
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+
     letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
